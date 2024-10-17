d693dbaa559d3943c1a2366b1511b25eb649b4cf
S R Pranav Suriya <pranavsuriya.sr@gmail.com>
2024-10-17 23:32:01 +0530

    Added day 1 and 2

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ function updateDashboard(data) {
     isSameDay(new Date(user.timestamp), selectedDate)
   );
 
-  totalEntriesElement.textContent = `Total Entries: ${filteredUsers.length}`;
+  totalEntriesElement.textContent = `Day 2 Total Entries: ${filteredUsers.length}`;
 
   // Clear existing rows
   scannedDataBody.innerHTML = '';
